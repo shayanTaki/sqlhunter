5cd4defff8b60d6ecb547442874c31cce8fcb0d5
shayan taki <76600102+shayanTaki@users.noreply.github.com>
2025-01-12 14:16:13 -0800

    def init class

--- sqlhunter.py
+++ sqlhunter.py
@@ -8,3 +8,18 @@ def animated_print(text, delay=0.1):
         print(char, end='', flush=True)
         time.sleep(delay)
     print()
+
+class SQLInjectionTester:
+    def __init__(self, url, method="GET", data=None, headers=None, cookies=None, delay=0, timeout=10):
+        """
+        shirdalcode.ir
+        """
+        self.url = url
+        self.method = method.upper()
+        self.data = data
+        self.headers = headers or {}
+        self.cookies = cookies or {}
+        self.delay = delay
+        self.timeout = timeout
+        self.payloads = self.generate_payloads()
+        self.vulnerabilities = {} # Dictionary to store vulnerabilities and errors
